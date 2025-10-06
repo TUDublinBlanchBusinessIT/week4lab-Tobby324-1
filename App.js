33e2fea4cc952e701aa94a29fcc0071b5119f91f
B00165442 Tobby Akinwale <B00165442@mytudublin.ie>
2025-10-06 16:21:58 +0100

    this is the click me button

--- App.js
+++ App.js
@@ -1,45 +1,45 @@
 import { SafeAreaView, Text, View, TextInput, Button, Alert, StyleSheet } from 'react-native';
-import {useState} from 'react';
+import { useState } from 'react';
 
 export default function App() {
   const gradePoints = {'F': 0,'D': 1.5,'C': 2,'C+': 2.75, 'B': 3, 'B+': 3.5,  'A': 4 };
-  
-  const [sswd, setSswd] = useState('D');
+
+  const [math, setmath] = useState('D');
   const [ob, setOb] = useState('D');
-  
-  var gpa=0;
-  var credits=5;
+
+  var gpa = 0;
+  var credits = 5;
   var totalPossibleCredits = 10;
   var totalGradeScores = 0;
-  
-  function clickMe(){
-    alert("this is the click me button"); //alert for web
-    Alert.alert("this is the click me button"); //alert for phone
-    //get the studnet's gradePointsfor SSWD, multiply it by the credits 
-    //add the result to totalGradeScores as an accumulator variable (e.g. tgs=tgs+sswd)
 
-    
-    //get the student's gradePointsfor Ob, multiply it by the credits
-    //add the result to totalGradeScores as an accumulator variable (e.g. tgs=tgs+ob)
-   
-    //calculate the gpa as the totalsGradeScores divided by the totalPossibleCredits
-    //Output the calculated GPA result to the user using an alert (you must concatenate the gpa)
+  function clickMe() {
+    alert("this is the click me button"); // Web alert
+    Alert.alert("this is the click me button"); // Phone alert
+
+    mathgpa = gradePoints[math];
+    mathscore = mathgpa + credits;
+
+    totalGradeScores = totalGradeScores + (gradePoints[math]*credits);
+    totalGradeScores = totalGradeScores + (gradePoints[ob]*credits);
+
+    gpa = totalGradeScores/10;
+
+    mathgpa = gradePoints[{math}];
+    alert('gpa is ='+gpa)
   }
-
-
-return (
+  return (
     <SafeAreaView>
-      <View><Text style={{flexDirection: "row", fontWeight: "bold", fontSize: 24, textAlign:"center", marginTop: "%10"}}>GPA Calculator</Text></View>
+      <View><Text style={{ flexDirection: "row", fontWeight: "bold", fontSize: 24, textAlign: "center", marginTop: "%10" }}>GPA Calculator</Text></View>
       <View>
         <Text>Maths</Text>
-        <TextInput  placeholder="Grade" onChangeText={setSswd}/>
+        <TextInput placeholder="Grade" onChangeText={setmath} />
       </View>
       <View >
         <Text>Organisational Behaviour</Text>
-        <TextInput placeholder="Grade" onChangeText={setOb)}/>
+        <TextInput placeholder="Grade" onChangeText={setOb} />
       </View>
       <View >
-        <Button title="submit" onPress={clickMe}/>
+        <Button title="submit" onPress={clickMe} />
       </View>
     </SafeAreaView>
   );
